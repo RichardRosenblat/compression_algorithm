3a1eb7395f5b33ea40fd894d9dabebe1189e78d0
Richard Rosenblat <rosenblatr@gmail.com>
2024-11-16 17:55:25 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+const file64String = '1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890'; 
+// ^^100 chars
+
+type patternAnalysisRecord = {
+    count: number;
+    size: number;
+}
+
+function PatternAnalysis(file64String:string) {
+    
+    const patternDict: Record<string, patternAnalysisRecord> = {};
+    const maxPatternLength = Math.trunc(file64String.length / 2);
+
+    let sizeOfPattern = 2
+
+   // for (let sizeOfPattern = 2; sizeOfPattern <= maxPatternLength; sizeOfPattern++) {
+        for (let i = 0; i < file64String.length; i++) {
+            let currentPattern = file64String.substring(i, i + sizeOfPattern);
+            if (currentPattern.length < sizeOfPattern) {
+                break   
+            }
+            console.log(currentPattern);
+        }
+   // }
+
+}
+
+PatternAnalysis(file64String);
